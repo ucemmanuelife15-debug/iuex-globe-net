8f7ae91d90304e9f775ce05e0b81b0dcaaf25495
EmmanuelIFeanyichukwu <ucemmanuelife15@gmail.com>
2026-09-21 03:59:42 +0100

    Increase server request size limit for profile pictures

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 const PORT = 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/game", gameRoutes);
